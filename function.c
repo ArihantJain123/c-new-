dfd27fb3126601cdd62167c1b87bfd3d8fb90aba
Arihant Jain <56069301+ArihantJain123@users.noreply.github.com>
2021-07-08 15:55:55 +0530

    new

---
+++ function.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void message();/* function declaration*/
+ int main()
+{
+    message();
+    printf("every thing is possible\n ");/*calling the function */
+    return 0;
+
+
+
+}
+void message ()   /*define a function */
+{
+
+    printf("smile is the beauty of world \n");
+
+}
