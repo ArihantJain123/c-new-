cef6bcbb4504b638334025585fb6a5bd0c54e926
Arihant Jain <56069301+ArihantJain123@users.noreply.github.com>
2021-07-09 11:43:07 +0530

    new two

---
+++ array10.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int array[4][2] = {
+                         {12 , 46},
+                         {23 , 64},
+                         {13 , 45},
+                         {19 , 49}
+
+                    };
+
+    int i,j;
+    for( i=0; i<=3; i++)
+    {
+        for(j=0;j<=1;j++)
+            printf("%d",*(*( array + i )+ j ));
+        printf("\n");
+
+
+    }
+    return 0;
+
+
+
+
+
+}
