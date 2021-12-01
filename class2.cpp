fa404e94c01753a4ae320bccbfa9bb95b496bc3f
Arihant Jain <56069301+ArihantJain123@users.noreply.github.com>
2021-12-01 13:15:41 +0530

    class

---
+++ class2.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+int a = 10;
+
+int main()
+{
+   system("cls");
+
+
+  int a = 15;
+cout<<" Main() " <<"\n";
+cout<<" a = "<<a<<"\n";
+cout<<"::a = "<<::a<<"\n";
+{
+    int a = 25;
+    cout<<"Inner Block\n";
+    cout<<"a = "<<a<<"\n";
+    cout<<"::a= "<<::a<<"\n";
+
+}
+cout<<"Back to main()"<<"\n";
+cout<<"a= "<<a<<"\n";
+cout<<"::a="<<::a<<"\n";
+return 0;
+
+
+}
