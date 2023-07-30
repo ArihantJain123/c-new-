e56986faa3630de0a4cbdc10ba17b23e22b8b9bf
Arihant Jain <56069301+ArihantJain123@users.noreply.github.com>
2023-07-30 22:30:46 +0530

    Update 2c++prog.cpp
    ...

--- 2c++prog.cpp
+++ 2c++prog.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+...#include<iostream>
 #include<math.h>
 #include<stdlib.h>
 //new here like here................. ......................................................................................................
