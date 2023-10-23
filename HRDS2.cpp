d1e050989ff28621be63c5e40bc85611788ffd48
Arihant Jain <56069301+ArihantJain123@users.noreply.github.com>
2023-10-23 21:45:10 +0530

    Update HRDS2.cpp
    ....

--- HRDS2.cpp
+++ HRDS2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <sstream>
 using namespace std;
-//Edit file new here new yes ................... h...ere .......
+//Edit file new here new yes ................... h...ere ..........
 class Student{
   private:
   int age,standard;
